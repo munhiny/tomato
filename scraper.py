706514d16b6890860b3237eded0c1fecb76f58fe
unknown <edmondyeois@gmail.com>
2020-01-27 18:26:27 +1100

    creating dfs

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from bs4 import BeautifulSoup
-from urllib.request import urlopen, getcode
+from urllib.request import urlopen
 
 
 def Get_Web_Page(address):
@@ -10,48 +10,83 @@ def Get_Web_Page(address):
 	page = html.read()
 	return(page)
 
+df = pd.DataFrame(columns = ['c_name', 'c_date', 'c_review', 'c_link', 'c_score'])
+
 a = Get_Web_Page("https://www.rottentomatoes.com/m/goldeneye/reviews")
 
 # Parse a
 
 soup = BeautifulSoup(a, 'html.parser')
 
+c_name = []
+c_date = []
+c_review = []
+c_score = []
+c_link = []
+
 # find tables
 tables = soup.find_all("div", class_="row review_table_row")
 for table in tables:
 	for name in table.find_all("a", class_="unstyled bold articleLink"):
-		print(name.get_text())
+		c_name.append(name.get_text())
 	for date in table.find_all("div", class_="review-date subtle small"):
-		print(date.get_text())
+		c_date.append(date.get_text())
+	for review in table.find_all("div", class_="the_review"):
+		c_review.append(review.get_text().strip())
 	for link in table.find_all("div", class_="small subtle review-link"):
-		for i in link.find_all('a'):
-			print(i.get('href'))
+		if not link.get_text():
+			c_score.append("") 
+		else:
+			c_score.append(link.get_text().strip())
+		if "Full Review" not in link.get_text():
+			c_link.append("")
+		else:
+			for i in link.find_all('a'):
+				c_link.append(i.get('href').strip())
+print (len(c_name)) 
+print (len(c_date)) 
+print (len(c_review)) 
+print (len(c_score)) 
+print (len(c_link)) 
+
+df = pd.DataFrame(
+	{'Critic_Name': c_name,
+	'Review_Date': c_date,
+	'Review': c_review,
+	'Score': c_score,
+	'Review_Link': c_link})
+print(df)
+
+#print(df['Review_Link'])
+
+df = df[df.Score.apply(lambda x: x.isnumeric())]
+print(df['Score'])
 
 
 
 
 
-	print(table)
+#print(table)
 
 # to find name of critics 
 
-names = soup.find_all("a", class_ = "unstyled bold articleLink")
+#names = soup.find_all("a", class_ = "unstyled bold articleLink")
 
-names_list = []
-for name in names:
-	names_list.append(name.get_text())
+#names_list = []
+#for name in names:
+#	names_list.append(name.get_text())
 
 # to find the second page of this rotten tomatoes critic reviews
-reviews_link_list = []
-reviews_link = soup.find_all("div", class_= "small subtle review-link")
-for i in reviews_link:
-	for links in (i.find_all('a')):
-		reviews_link_list.append(links.get('href'))
+#reviews_link_list = []
+#reviews_link = soup.find_all("div", class_= "small subtle review-link")
+#for i in reviews_link:
+#	for links in (i.find_all('a')):
+#		reviews_link_list.append(links.get('href'))
  
  #store links in array to compare if same or not
-reviews_link_list = []
-for link in reviews_link:
-	reviews_link_list.append(link.get('href')) # stores path as q so we can compare if there is more
+#reviews_link_list = []
+#for link in reviews_link:
+#	reviews_link_list.append(link.get('href')) # stores path as q so we can compare if there is more
 
 # Page 2 is /m/goldeneye/reviews?type=&sort=&page=2
 # concatonate this with base url? https://www.rottentomatoes.com/m/goldeneye/reviews?type=&sort=&page=2
